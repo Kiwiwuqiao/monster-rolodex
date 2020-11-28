aa8a436c4b36db9500c6b50a8595de30fd1fa7b6
Kiwiwuqiao <Kiwiwuqiao@Kiwis-Macbook-Pro.local>
2020-11-28 18:26:58 +0800

    adding files for github pages

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,12 @@
+
+
+import { Card } from '../card/card.compononent'
+
+import './card-list.styles.css'
+
+export const CardList = (props) => (
+
+        <div className="card-list">
+            {props.monsters.map(monster => <Card key={monster.id} monster={monster} />)}
+        </div>
+)
